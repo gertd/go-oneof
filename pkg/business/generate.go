a7c2c21b7a29055fbcd7481dd24c6ca471fec555
Gert Drapers <gert.drapers@live.com>
2019-07-31 10:23:32 -0700

    remove lint step from generate.go

--- pkg/business/generate.go
+++ pkg/business/generate.go
@@ -4,4 +4,3 @@ package business
 
 //go:generate gogen-enum -input ./addressKind.yaml -package business -output ./addressKind.go
 //go:generate gofmt -w addressKind.go
-//go:generate golangci-lint run addressKind.go
